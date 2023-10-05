6cb86945156f93be627478e25f3d6675c7c45b8e
Asteriuz <gutocebola@gmail.com>
2023-10-05 04:33:10 -0300

    Pre Carousel

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,33 +1,35 @@
-const prevButton = document.getElementById('default-carousel-prev');
-const nextButton = document.getElementById('default-carousel-next');
+const prevButton = document.getElementById("default-carousel-prev");
+const nextButton = document.getElementById("default-carousel-next");
 let canSlide = true;
 
-document.addEventListener('keydown', function (event) {
-    if (!canSlide) {
-        return;
-    }
-    if (event.key === 'ArrowLeft') {
-        prevButton.click();
-        canSlide = false;
-        setTimeout(() => {
-            canSlide = true;
-        }, 700);
-    } else if (event.key === 'ArrowRight') {
-        nextButton.click();
-        canSlide = false;
-        setTimeout(() => {
-            canSlide = true;
-        }, 700);
-    }
+document.addEventListener("keydown", function (event) {
+  if (!canSlide) {
+    return;
+  }
+  if (event.key === "ArrowLeft") {
+    prevButton.click();
+    canSlide = false;
+    setTimeout(() => {
+      canSlide = true;
+    }, 700);
+  } else if (event.key === "ArrowRight") {
+    nextButton.click();
+    canSlide = false;
+    setTimeout(() => {
+      canSlide = true;
+    }, 700);
+  }
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var arrowElement = document.querySelector('.scroll-down-arrow-hero');
+document.addEventListener("DOMContentLoaded", function () {
+  var arrowElement = document.querySelectorAll(".scroll-down-arrow-hero");
 
-    arrowElement.addEventListener('click', function () {
-        window.scrollTo({
-            top: window.innerHeight + 24,
-            behavior: 'smooth'
-        });
+  for (var i = 0; i < arrowElement.length; i++) {
+    arrowElement[i].addEventListener("click", function () {
+      window.scrollTo({
+        top: window.innerHeight + 24,
+        behavior: "smooth",
+      });
     });
+  }
 });
