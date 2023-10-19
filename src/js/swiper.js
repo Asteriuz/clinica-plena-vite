ad3f7c844ce050217460f4dfa72d63e83ab91a4c
Asteriuz <gutocebola@gmail.com>
2023-10-19 20:53:59 -0300

    Added Swiper.js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,8 +1,9 @@
 import Swiper from "swiper"
-import { Navigation, Pagination } from "swiper/modules"
-// import Swiper and modules styles
+import { Navigation, Pagination, Thumbs, EffectFade, FreeMode, Keyboard, Autoplay } from "swiper/modules"
 import "swiper/css"
 import "swiper/css/navigation"
+import "swiper/css/thumbs"
+import "swiper/css/effect-fade"
 
 // change swiper var to this;
 // :root {
@@ -14,13 +15,52 @@ import "swiper/css/navigation"
 //   }
 
 // init Swiper:
-const swiper = new Swiper(".swiper", {
-  modules: [Navigation, Pagination],
-  direction: "horizontal",
+const swiperThumbs = new Swiper(".swiper-thumbs", {
+  modules: [FreeMode, Thumbs],
   loop: true,
+  spaceBetween: 10,
+  freeMode: true,
+  autoScrollOffset: 1,
+  watchSlidesProgress: true,
+  breakpoints: {
+    // 3, 4, 5
+    320: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
+  },
+})
 
+const swiper = new Swiper(".swiper", {
+  modules: [Navigation, Pagination, Thumbs, EffectFade, Keyboard, Autoplay],
+  effect: "fade",
+  speed: 700,
+  grabCursor: true,
+  spaceBetween: 10,
+  fadeEffect: {
+    crossFade: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+
+  thumbs: {
+    swiper: swiperThumbs,
+  },
+  on: {
+    slideChange: function () {
+      swiperThumbs.slideToLoop(this.realIndex, 700, false)
+    },
   },
 })
