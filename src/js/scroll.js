d1044bae53c90beb932221deb23e5a808fc502b2
Asteriuz <gutocebola@gmail.com>
2023-10-18 22:44:39 -0300

    Added Scroll and Fixed Favicon

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -35,3 +35,20 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".obs-hidden")
 hiddenElements.forEach((hiddenElement) => observer.observe(hiddenElement))
+
+const body = document.querySelector("body")
+
+window.addEventListener(
+  "scroll",
+  () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      body.classList.add("scroll-bottom")
+    } else if (window.scrollY === 0) {
+      body.classList.add("scroll-top")
+    } else {
+      body.classList.remove("scroll-top")
+      body.classList.remove("scroll-bottom")
+    }
+  },
+  { passive: true }
+)
